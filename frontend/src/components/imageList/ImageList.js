5ef865c6cff3910fea792f1a7a1274088da7e423
Shoaib Dar <SDAR30@gmail.com>
2022-11-17 13:26:03 -0500

    added searchbar

--- frontend/src/components/imageList/ImageList.js
+++ frontend/src/components/imageList/ImageList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from '../imageCard/ImageCard';
+import SearchBar from '../searchBar/SearchBar';
 import './ImageList.scss'
 
 const ImageList = () => {
     const [images, setImages] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
     useEffect(() => {
         const URL = 'https://ai-art-backend.adaptable.app/images'
@@ -13,9 +15,21 @@ const ImageList = () => {
             })
     }, [])
 
+    let filteredImages = images;
+    if(searchTerm){
+        filteredImages = images.filter(image=>{
+            const search = searchTerm.toLowerCase();
+            const title = image.title.toLowerCase();
+            return title.split(' ').some(word => word.startsWith(search)) ? title : null;
+        })
+    }
+    console.log(filteredImages)
+
     return (
         <div className="imageList">
-            {images.map(image=> <ImageCard image={image} key={image.id} />)}
+            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+            {filteredImages.map(image=> <ImageCard image={image} key={image.id} />)}
+            {!filteredImages.length && <div className='imageList__noResults'>No results</div>}
         </div>
     );
 }
