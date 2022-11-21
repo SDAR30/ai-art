2a4b833ee0b125befb04636f3b183f344e566eb2
Shoaib Dar <SDAR30@gmail.com>
2022-11-21 11:44:14 -0500

    fied db config file part 2

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,19 +3,21 @@ require('dotenv').config();
 
 const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER  } = process.env;
 
-const cn = DATABASE_URL ? {
-    connectionString: DATABASE_URL,
-    max: 30,
-    ssl: {
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
         rejectUnauthorized: false,
-      }
-} : {
-    host: PG_HOST,
-    port: PG_PORT,
-    database: PG_DATABASE,
-    user: PG_USER
-}
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
 
-const db = pgp(cn);
+const db = pgp(cn)
 
 module.exports = db;
