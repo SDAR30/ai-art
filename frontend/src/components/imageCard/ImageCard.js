9a74980b3efb8b3694b62d36c80a1935af65215f
Shoaib Dar <SDAR30@gmail.com>
2022-12-27 20:13:04 -0500

    remove react-text fit package and manually fix font size on imageCard

--- frontend/src/components/imageCard/ImageCard.js
+++ frontend/src/components/imageCard/ImageCard.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useRef  } from 'react';
 import './ImageCard.scss';
 import { Link } from "react-router-dom";
-import ReactFitText from 'react-fittext';
 import { capitalizeFirstLetterOfEachWord, removeExtraWords } from '../../utils/stringUtils';
 import { timeSince } from '../../utils/dateUtils';
+import GetElementDimension from '../../utils/GetElementDimension';
 
 function ImageCard({ image }) {
+    const ref = useRef();
+    const {width, height} = GetElementDimension(ref);
     const { id, ai, prompt, title, url, date } = image;
+    const styles = {
+      title: {
+        fontSize: width ? width/22.8: 10,
+        color: '#ff6b0f'
+      },
+      date: {
+        fontSize: width ? width/26: 10,
+        marginTop: '3px',
+        color: 'grey'
+      },
+      prompt: {
+        fontSize: width ? width/23: 10,
+      },
+      ai: {
+        fontSize: width ? width/26: 10
+      }
+    }
+
 
     return (
-        <div className='imageCard'>
+        <div className='imageCard' >
             <Link className='imageCard__link' to={`/images/${id}`} state={{ image: image }}>
                 <img className="imageCard__image" src={url} alt="ai img" />
-                <div className="imageCard__overlay">
-                    <ReactFitText compressor={1.5}>
-                        <p className='imageCard__overlay__title'>{capitalizeFirstLetterOfEachWord(removeExtraWords(title, 30))}</p>
-                    </ReactFitText>
-                    <ReactFitText compressor={.66}>
-                        <p className='imageCard__overlay__date'>{timeSince(date)}</p>
-                    </ReactFitText>
-                    <ReactFitText compressor={2}>
-                        <p className='imageCard__overlay__prompt'>{removeExtraWords(prompt, 80)}</p>
-                    </ReactFitText>
-                    <ReactFitText compressor={2}>
-                        <p className='imageCard__overlay__ai'>made with {ai}</p>
-                    </ReactFitText>
+                <div className="imageCard__overlay" ref={ref} >
+                    <p className='imageCard__overlay__title' style={styles.title}>{capitalizeFirstLetterOfEachWord(removeExtraWords(title, 28))}</p>
+                    <p className='imageCard__overlay__date' style={styles.date}>{timeSince(date)}</p>
+                    <p className='imageCard__overlay__prompt' style={styles.prompt}>{removeExtraWords(prompt, 80)}</p>
+                    <p className='imageCard__overlay__ai' style={styles.ai}>made with {ai}</p>
+                    <p className='imageCard__overlay__dimensions' style={styles.ai}>{width} : {height}</p>
                 </div>
             </Link>
         </div>
