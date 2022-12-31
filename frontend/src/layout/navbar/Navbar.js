e72fb3aaf04e621ca7937018a30c7a00bd8b3189
Shoaib Dar <SDAR30@gmail.com>
2022-12-31 15:43:30 -0500

    add dimensions file for media queries

--- frontend/src/layout/navbar/Navbar.js
+++ frontend/src/layout/navbar/Navbar.js
@@ -9,30 +9,6 @@ import AddCircle from '@mui/icons-material/AddCircle';
 function Navbar({ setOpenLoginModal, isLoggedIn, setIsLoggedIn }) {
     const [active, setActive] = useState(false)
     const { user, setUser } = useContext(UserContext);
-    // const styles ={
-    //     loginButton:{
-    //         borderRadius: '20px',
-    //         fontSize: '.8rem',
-    //         fontWeight: 'bold',
-    //         margin: '0 .5rem',
-    //         padding: '.2rem .5rem',
-    //         color: 'primary.contrastText',
-    //         '&:hover': {
-    //             backgroundColor: 'primary.main',
-    //             opacity: [0.9, 0.8, 0.7],
-    //         }
-    //     },
-    //     signupButton:{
-    //         borderRadius: '20px',
-    //         fontSize: '.8rem',
-    //         fontWeight: 'bold',
-    //         padding: '.2rem .5rem',
-    //         '&:hover': {
-    //             backgroundColor: 'background.paper',
-    //             opacity: [0.9, 0.8, 0.7],
-    //         }
-    //     }
-    // }
 
     const openLoginButton = () => {
         setIsLoggedIn(false)
@@ -47,23 +23,25 @@ function Navbar({ setOpenLoginModal, isLoggedIn, setIsLoggedIn }) {
         <div className="navbar">
 
             <div className="navbar__logo"><NavLink to="/">AI ART</NavLink ></div>
-
-            <div className={active ? "navbar__menuItems-active" : "navbar__menuItems"} onClick={() => setActive(false)}>
-                <li className="navbar__menuItem"> <NavLink to="/">Gallery</NavLink ></li>
-                <li className="navbar__menuItem"><NavLink to="/about">About</NavLink></li>
-                <li className="navbar__menuItem"><NavLink to="/contact">Contact</NavLink></li>
-                {user && <li className="navbar__menuItem"><NavLink to="/images/new">Upload</NavLink ></li>}
-
-            </div>
             <div className="navbar__toggleIcon" onClick={() => setActive(!active)}>=</div>
+            <div className='navbar__buttons'>
 
-            {!user ?
-                <div className="navbar__buttons">
-                    <button className="navbar__loginButton navbar__button" onClick={openLoginButton}><LoginIcon /><span>LOG IN</span></button>
-                    <button className="navbar__signupButton navbar__button" onClick={openSignupButton}><span>SIGN UP</span> <AddCircle /></button>
-                </div> :
-                <ProfileIcon setActive={setActive} setUser={setUser} />
-            }
+                <div className={active ? "navbar__menuItems-active" : "navbar__menuItems"} onClick={() => setActive(false)}>
+                    <li className="navbar__menuItem"> <NavLink to="/">Gallery</NavLink ></li>
+                    <li className="navbar__menuItem"><NavLink to="/about">About</NavLink></li>
+                    <li className="navbar__menuItem"><NavLink to="/contact">Contact</NavLink></li>
+                    {user && <li className="navbar__menuItem"><NavLink to="/images/new">Upload</NavLink ></li>}
+
+                </div>
+
+                {!user ?
+                    <div className="navbar__logButtons">
+                        <button className="navbar__loginButton navbar__logButton" onClick={openLoginButton}><LoginIcon /><span>LOG IN</span></button>
+                        <button className="navbar__signupButton navbar__logButton" onClick={openSignupButton}><span>SIGN UP</span> <AddCircle /></button>
+                    </div> :
+                    <ProfileIcon setActive={setActive} setUser={setUser} />
+                }
+            </div>
 
         </div>
     );
