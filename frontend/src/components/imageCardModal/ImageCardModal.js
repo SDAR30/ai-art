58ca1b63aff09eb724bcc4ae4cf602abcc5b747c
Shoaib Dar <SDAR30@gmail.com>
2023-01-27 22:00:52 -0500

    added close and expand buttons to imageCardModal

--- frontend/src/components/imageCardModal/ImageCardModal.js
+++ frontend/src/components/imageCardModal/ImageCardModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Box from '@mui/material/Box';
+//import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import './ImageCardModal.scss'
 import { timeSince } from '../../utils/dateUtils';
@@ -10,6 +10,8 @@ import { useCookies } from 'react-cookie';
 import { saveAs } from 'file-saver';
 import DownloadIcon from '@mui/icons-material/Download';
 import Tooltip from '@mui/material/Tooltip';
+import CloseIcon from '@mui/icons-material/Close';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
 
 function ImageCardModal({ openCardModal, setOpenCardModal, image, imgObj }) {
     const { id, ai, date, prompt, title, instructions, url, avg_rating } = image;
@@ -20,6 +22,15 @@ function ImageCardModal({ openCardModal, setOpenCardModal, image, imgObj }) {
     const user_id = cookies.token ? cookies.user.id : 0;
     const [imageDimensions, setImageDimensions] = useState({});
     const textRef = useRef(null);
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleImageSize = () =>{
+        //imgRef.current.style.width = '100%';
+        setExpanded(!expanded);
+
+        //open image in new tab
+        //onClick={() => window.open(image.url)} 
+    }
 
     //to copy text to clipboard, create a textarea element, set its value to the text, select it, and copy it
     //then remove the textarea element
@@ -32,7 +43,7 @@ function ImageCardModal({ openCardModal, setOpenCardModal, image, imgObj }) {
         document.execCommand("copy");
         textArea.remove();
         alert("Text copied to clipboard!");
-      }
+    }
 
     useEffect(() => {
         imgObj.onload = () => {
@@ -84,6 +95,7 @@ function ImageCardModal({ openCardModal, setOpenCardModal, image, imgObj }) {
 
     const handleClose = () => {
         setOpenCardModal(false);
+        setExpanded(false);
     }
 
     return (
@@ -93,33 +105,37 @@ function ImageCardModal({ openCardModal, setOpenCardModal, image, imgObj }) {
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description">
-            <Box className='imageCardBox'>
-                <h2 className='imageCardBox__title'> {title} </h2>
-                <img className='imageCardBox__img' src={url} alt="modal view" />
-                <div className='imageCardBox__details'>
-                    <div className='imageCardBox__artist'>
-                        <img src={artist.pic} alt='profile'></img> <div>by {artist.username}</div>
-                    </div>
-                    <div className='imageCardBox__prompt'>
-                        <span>prompt: </span><span title='copy' ref={textRef} onClick={copyText}>{prompt}</span>
+            <div>
+            <Tooltip title="close image"><CloseIcon className='imageCardModal__close' onClick={handleClose} fontSize='large' /></Tooltip>
+                <div className={expanded ? 'imageCardBox-expanded imageCardBox' : 'imageCardBox'}>
+                <Tooltip title="expand image"><FullscreenIcon className='imageCardBox__fullscreen' onClick={toggleImageSize}  /></Tooltip>
+                    <h2 className='imageCardBox__title'> {title} </h2>
+                    <img className={expanded ? 'imageCardBox__img-expanded imageCardBox__img' : 'imageCardBox__img'}  src={url} alt="modal view" />
+                    <div className='imageCardBox__details'>
+                        <div className='imageCardBox__artist'>
+                            <img src={artist.pic} alt='profile'></img> <div>by {artist.username}</div>
                         </div>
-                    {instructions && <div className='imageCardBox__instructions'>-- {instructions}</div>}
+                        <div className='imageCardBox__prompt'>
+                            <span>prompt: </span><span title='copy' ref={textRef} onClick={copyText}>{prompt}</span>
+                        </div>
+                        {instructions && <div className='imageCardBox__instructions'>-- {instructions}</div>}
 
-                    <div className='imageCardBox__date' >created {timeSince(date)} using {ai}</div>
+                        <div className='imageCardBox__date' >created {timeSince(date)} using {ai}</div>
 
-                    <div className='imageCardBox__dimensions' >
+                        <div className='imageCardBox__dimensions' >
                             <Tooltip title="Download Image"><DownloadIcon onClick={downloadImage} /></Tooltip>
-                        <div>{imageDimensions.width} x {imageDimensions.height}</div>
+                            <div>{imageDimensions.width} x {imageDimensions.height}</div>
+                        </div>
+
+
+
                     </div>
-
-
-
+                    <div className='imageCardBox__rating'>
+                        <div>Rating</div>
+                        <HoverRating rating={currentAvgRating} submitRating={submitRating} />
+                    </div>
                 </div>
-                <div className='imageCardBox__rating'>
-                    <div>Rating</div>
-                    <HoverRating rating={currentAvgRating} submitRating={submitRating} />
-                </div>
-            </Box>
+            </div>
         </Modal>
     );
 }
