bf4e133b1c097ee22e48ba10367f002de2da8142
Shoaib Dar <SDAR30@gmail.com>
2023-02-27 20:22:27 -0500

    adjusted spacing for searchbar and sortbar in main page

--- frontend/src/components/selectBar/SelectBar.js
+++ frontend/src/components/selectBar/SelectBar.js
@@ -4,7 +4,7 @@ import './SelectBar.scss';
 function SelectBar({selectedOption, handleSelectChange}) {
     return (
             <select className='selectBar' value={selectedOption} onChange={handleSelectChange}>
-                <option value="">Sort By</option>
+                <option value="" >Sort Images By</option>
                 <option value="1">Latest</option>
                 <option value="2">Oldest</option>
                 <option value="3">Rating</option>
